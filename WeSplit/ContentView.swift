c3472d2011e06de39cbb3c68e4769ef8a2c57231
acifre <78503928+acifre@users.noreply.github.com>
2023-01-22 20:38:01 -0600

    Add text field and picker

--- WeSplit/ContentView.swift
+++ WeSplit/ContentView.swift
@@ -8,12 +8,47 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    let tipPercentages = [0, 10, 15, 18, 20, 25]
+    
+    @State private var checkAmount = 0.0
+    @State private var numberOfPeople = 2
+    
+    
+    @State private var tapCount = 0
+    @State private var name = ""
+    @State private var chosenPercentage = 18
+    
     var body: some View {
-        VStack {
+        NavigationView {
+            VStack {
+                Form {
 
-            Text("WeSplit")
+                    Section("Check Amount") {
+                        TextField("Check Amount", value: $checkAmount, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
+                            .keyboardType(.decimalPad)
+                            .padding()
+                    }
+                    
+                    Section {
+                        Text(checkAmount, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
+                            .padding()
+                    }
+                    
+                    Section("Tip Percentage") {
+                        Picker("Select your tip percentage", selection: $chosenPercentage) {
+                            ForEach(tipPercentages, id: \.self) { number in
+                                Text(String(number))
+                            }
+                        }
+                        .padding()
+                    }
+                }
+
+            }
+            .navigationTitle("WeSplit")
+            .navigationBarTitleDisplayMode(.inline)
         }
-        .padding()
     }
 }
 
