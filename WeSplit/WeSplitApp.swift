66b29169ba858383695ad399227fc78bba8397e5
acifre <78503928+acifre@users.noreply.github.com>
2023-01-22 20:02:59 -0600

    Initial Commit

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Anthony Cifre on 1/22/23.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
